e007f4956a12dd412d99fea90744d5d606f6ba5e
Thomas Blood <132237499+N0Heart@users.noreply.github.com>
2023-05-31 17:18:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Imported Libraries #
 import discord
 import requests
-import asyncio
 import yt_dlp as youtube_dl
+import asyncio
 from discord.ext import commands
+
 # Reassignment Keys #
 
 intents = discord.Intents().all()
@@ -21,10 +22,18 @@ ffmpeg_options = {'options': '-vn'}
 
 # Functions #
 # Gets random insult from evilinsult api #
-def get_quote():
+def get_ins():
     response = requests.get("https://evilinsult.com/generate_insult.php?")
     quote = response.text
     return quote
+
+
+def get_comp():
+    response = requests.get("https://complimentr.com/api")
+    data = response.json()
+    compliment = data["compliment"]
+    capitalize = compliment.capitalize()
+    return capitalize + "."
 
 
 typing = ["fast", "speed", "typing", 'keyboard', "switches", "quick", "type"]
@@ -37,11 +46,20 @@ greetings = ["Hi", "hi", "hello", "sup", "whats good", "whats up", "wya"]
 
 doug = ["doug", "thomas", "tommy", "tom", "doog"]
 
+commands = ["!ban - lists banned words", "!comp - generates a random compliment", "!info - lists current server info",
+            "!music - music player instructions", "!wave - wave at a user"]
+
 
 # Client Events #
 @bot.command()
 async def ban(ctx):
     await ctx.send(f"Banned words are: {', '.join(banned)}")
+
+
+@bot.command()
+async def comp(ctx):
+    quote = get_comp()
+    await ctx.send(quote)
 
 
 @bot.command()
@@ -67,7 +85,7 @@ async def wave(ctx, arg):
 @bot.event
 async def on_ready():
     print("Holy smokes I'm logged in to Discord as {0.user}! Get ready for extreme moderation.".format(bot))
-    channel = bot.get_channel(xxxxxxxxxxxxxxxxxxx)
+    channel = bot.get_channel(1102412567659425835)
     await channel.send(
         "Holy smokes I'm logged in to Discord as {0.user}! Get ready for extreme moderation.".format(bot))
 
@@ -83,7 +101,7 @@ async def on_message(message):
     # If a user mentions the bot #
     if bot.user.mentioned_in(message):
         await message.channel.send(
-            f"Hello {user.mention}! How are you doing today? Want me to play some music? Just type !play followed by a "
+            f"Hello {user.mention}! How are you doing today? Want me to play some music? Just type $play followed by a "
             "space and a YouTube link to the media you want to hear!")
 
     # If a user talks about Thomas #
@@ -95,12 +113,12 @@ async def on_message(message):
 
     # Creates a random insult #
     if message.content.lower().startswith("!insult"):
-        quote = get_quote()
+        quote = get_ins()
         await message.channel.send(quote)
     # Banned word action #
     if any(word in message.content.lower() for word in banned):
-        get_quote()
-        s = get_quote()
+        get_ins()
+        s = get_comp()
         await message.channel.send(
             f"{user.mention} {s} It is so important that you understand that this kind of language is absolutely NOT "
             f"allowed in my discord server. Try that again and you will be permanently removed.")
@@ -131,7 +149,7 @@ async def on_message(message):
             voice_client = await message.author.voice.channel.connect()
             voice_clients[voice_client.guild.id] = voice_client
         except:
-            await message.channel.send("Error playing file.")
+            await message.channel.send("There could be an error playing the file. Give me a second...")
 
         try:
             url = message.content.split()[1]
@@ -170,4 +188,4 @@ async def on_message(message):
 
 
 # Client Key - DO NOT TOUCH OR EDIT BELOW THIS LINE - #
-bot.run(xxx)
+bot.run("xxx")
